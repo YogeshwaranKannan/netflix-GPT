bd63ee6e040c6d129f08459dc39c5b20beba07b1
YOGESWARAN KANNAN <yogeshgimmy07@gmail.com>
2024-03-09 10:39:31 +0530

    last commit changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,11 @@ export const API_OPTION = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODViMDg0NGE5ZWE5MzMyMDFlOTk4NjA4OTZkMjVlNCIsInN1YiI6IjY1ZTdkNDMxZWE0MjYzMDE2MjIxM2ZjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3cizrz7py0XXvOoMUpfEUJEMIJz4URSwFI3GpN-jv4k'
+    Authorization: 'Bearer' + process.env.React_APP_OPENAI_CDN,
   }
 };
+
 export const IMG_CDN ="https://image.tmdb.org/t/p/w500/";
 export const BACK_CDN="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg";
-export const OPENAI_CDN="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";
+export const OPENAI_CDN= process.env.React_APP_OPENAI_CDN;
   
